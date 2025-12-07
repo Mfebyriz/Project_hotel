b96a40fa489a8520145faa86fd69c49b1b3149e1
Feby <mfebyrizki@gmail.com>
2025-12-07 20:05:02 +0700

    customer: home, booking, room_detail

--- hotel_reservation/lib/screens/auth/register_screen.dart
+++ hotel_reservation/lib/screens/auth/register_screen.dart
@@ -3,7 +3,7 @@ import 'package:hotel_reservation/services/auth_service.dart';
 import 'package:hotel_reservation/screens/customer/customer_home_screen.dart';
 
 class RegisterScreen extends StatefulWidget {
-  const RegisterScreen({Key? key}) : super(key: key);
+  const RegisterScreen({super.key});
 
   @override
   State<RegisterScreen> createState() => _RegisterScreenState();
